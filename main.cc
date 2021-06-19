aabdcae0608cec8e35b0244882e67b7902afa2c3
Zameer Bharwani <zameerbharwani@hotmail.com>
2021-06-19 00:45:29 -0400

    up to but not including chapter 4.2

---
+++ main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "color.h"
+#include "vec3.h"
+
+using namespace std;
+
+int main() {
+
+    const int image_width = 256;
+    const int image_height = 256;
+
+    cout<<"P3"<<endl<<image_width<< ' '<<image_height<<endl<<"255"<<endl;
+
+    for (int j = image_height -1; j>=0; --j) {
+        cerr<<"\rScanlines remaining: " << j << ' '<<flush;
+        for (int i = 0; i < image_width; ++i) {
+            color pixel_color(double(i)/(image_width -1), double(j)/(image_height-1), 0.25);
+            write_color(cout, pixel_color);
+        }
+    }
+    cerr<<endl<<"Done."<<endl;
+}
