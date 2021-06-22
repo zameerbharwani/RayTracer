26b4d36e2a271f053ea10ad59594fe46845fb1e5
Zameer Bharwani <zameerbharwani@hotmail.com>
2021-06-21 20:06:35 -0400

    produced a simple red sphere

--- main.cc
+++ main.cc
@@ -5,7 +5,31 @@
 
 using namespace std;
 
-color ray_color(const ray& r) {
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+
+    /*
+       A := point of origin of the ray 
+       b := direction vector of the ray
+       C := center of the sphere
+
+       0 = t^2 * b^2 + 2*t*b*(A-C) + (A-C)*(A-C) - r^2
+       coeff_1  = b^2
+       coeff_2 = 2*b*(A-C) 
+       coeff_3 = (A-C)*(A-C) - r^2
+       Quadratic in terms of t
+    */
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+color ray_color(const ray &r) {
+    
+    if (hit_sphere(point3(0,0,-1),0.5,r)) return color(1,0,0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
